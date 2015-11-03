c4e1e1d6cdbd4a6f33195ac8daff0382795f0ded
wilhelmryan <jhiscockwagner@yahoo.com>
2015-11-02 20:38:34 -0800

    Create 06.cpp

---
+++ Chapter-5/06.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+
+    for(x=0;x<=20;x++)
+    {
+        cout<<"The square of "<<x<<" is "<<x*x<<endl;
+    }
+    return(0);
+}

84b40adf3b0b29ddb203f54797a9e4ae61eda158
wilhelmryan <jhiscockwagner@yahoo.com>
2015-11-02 20:27:16 -0800

    Create 02.cpp

---
+++ Chapter-3/02.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int val_a=0;
+    int val_b=0;
+    cout << "Input a number: ";
+    cin >> val_a;
+    cout << "Input a second number: ";
+    cin >> val_b;
+    cout << "The two numbers added together equal: " << val_a + val_b<< endl;
+    return 0;
+}

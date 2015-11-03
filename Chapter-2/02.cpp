8c32cd287e8874bc0251fe5dcab7755cc6a6f384
wilhelmryan <jhiscockwagner@yahoo.com>
2015-11-02 20:26:23 -0800

    Create 02.cpp

---
+++ Chapter-2/02.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "This is my name in the correct language: Jarrod \n And this has two lines in one comment" << endl;
+   // cout << "And I know how comments work from C...";
+    return 0;
+}
